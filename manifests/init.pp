616d125bf2973c408003f0dbcb2c92e798d2be65
Lukas Kallies <kallies@puzzle.ch>
2016-11-23 07:14:53 +0100

    Fix copy&paste issue.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 3 as published by
-# the Free Software Foundation.class
+# the Free Software Foundation.
 
 class x2go($version = 'installed') {
   include x2go::common
