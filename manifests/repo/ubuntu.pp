2f3a94e9ac467642a9ab8820af36392d477d754a
Lukas Kallies <kallies+fam@puzzle.ch>
2016-12-01 14:34:56 +0100

    Remove trusty, will be added automatically.

--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -4,7 +4,7 @@ class x2go::repo::ubuntu {
   include apt
   apt::source { 'x2go':
     location    => 'http://ppa.launchpad.net/x2go/stable/ubuntu',
-    repos       => 'trusty main',
+    repos       => 'main',
     include_src => false,
     key         => {
       'id'     => 'a7d8d681b1c07fe41499323d7cde3a860a53f9fd',
