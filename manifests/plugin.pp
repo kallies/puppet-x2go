ceb88153e962624cdb15a0af241aac20775577df
selyx <selyx@users.noreply.github.com>
2016-12-06 07:54:10 +0100

    change defined type plugin to class

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -3,7 +3,7 @@
 # This program is free software; you can redistribute it and/or modify it 
 # under the terms of the GNU General Public License version 3 as published by
 # the Free Software Foundation.class 
-define x2go::plugin (
+class x2go::plugin (
   $ensure = present,
 ) {
   include x2go::repo
